58460eaed74730f14ddf5c5aa07c1af03acbb628
Marek Salat <salat.marek42@gmail.com>
2014-04-04 17:00:47 +0300

    druhy pokus na test app

---
+++ data/env/Scripts/gearbox-script.py
@@ -0,0 +1,10 @@
+#!C:\tg2env\openshift\trine\data\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'gearbox==0.0.6','console_scripts','gearbox'
+__requires__ = 'gearbox==0.0.6'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('gearbox==0.0.6', 'console_scripts', 'gearbox')()
+    )
