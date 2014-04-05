f6ae03a11fc9ac218c6cfaf7ca485597e7e5564a
Marek Salat <salat.marek42@gmail.com>
2014-04-05 16:00:10 +0300

    mensi opravy

--- wsgi/tg2app/trine/controllers/root.py
+++ wsgi/tg2app/trine/controllers/root.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Main Controller"""
 #from routes import Mapper
+from routes import Mapper
 
 from tg import expose, flash, request, tmpl_context
 from tg.i18n import ugettext as _
@@ -9,6 +10,7 @@ from trine.controllers.tgroot import TGRootController
 from trine.lib.base import BaseController
 from trine.controllers.error import ErrorController
 from trine.controllers.FundController import FundController
+from trine.model import DBSession
 
 __all__ = ['RootController']
 
@@ -34,21 +36,21 @@ class RootController(BaseController):
     def __init__(self):
         self._fund = FundController(DBSession)
         self._root = TGRootController()
-        # 
+
         # map = Mapper()
         # map.connect(None, "/error/{action}/{id}", controller="error")
         # map.connect("home", "/", controller="main", action="index")
         # # ADD CUSTOM ROUTES HERE
         # map.connect(None, "/{controller}/{action}")
         # map.connect(None, "/{controller}/{action}/{id}")
-        # 
+        #
         # self.map = map
 
 
     @expose()
     def _lookup(self, *args):
         path = request.environ['PATH_INFO']
-        flash(_(r'%s - %s  - %r') % (args, path, self.map.routematch(path)))
+        # flash(_(r'%s - %s  - %r') % (args, path, self.map.routematch(path)))
         return self._root, args
 
     def _before(self, *args, **kw):
