81ee4117e167761787760767abd5a361e5ddac3a
amertak <kadlec.david@gmail.com>
2014-07-09 21:47:30 +0200

    Tag selector

--- wsgi/tg2app/trine/public/javascript/services.js
+++ wsgi/tg2app/trine/public/javascript/services.js
@@ -1,3 +1,21 @@
-angular.module('trine').factory('TransactionService', ['$resource', function($resource) {
-	return $resource('/api/v1/quick-key/transaction/');
+angular.module('trine.services', [])
+
+.factory('TransactionService', ['$resource', function($resource) {
+
+return $resource('/api/v1/quick-key/transaction/', {}, {
+      get: {
+        method: 'GET',
+        params: { },
+        headers: { 'Content-Type': 'application/json;charset=utf-8' }
+      }
+    });
+}]).factory('TagsService', ['$resource', function($resource) {
+
+return $resource('/api/v1/quick-key/tag/', {}, {
+      get: {
+        method: 'GET',
+        params: { },
+        headers: { 'Content-Type': 'application/json;charset=utf-8' }
+      }
+    });
 }]);
