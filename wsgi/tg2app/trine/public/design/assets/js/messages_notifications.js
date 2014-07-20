310e943ca76d81047ce83ce1a59d102ed9cb6828
Marek Salat <salat.marek42@gmail.com>
2014-07-20 18:17:22 +0200

    ignorovani symbolickeho adresare design/assets

--- wsgi/tg2app/trine/public/design/assets/js/messages_notifications.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$('#popover').popover();$('.tip').tooltip();$('#btnFlip').click(function(){$('#animateAlert').show();$('#animateAlert').addClass("bounceIn ");});$('#btnShake').click(function(){$('#animateAlert').show();});$('#btnBouce').click(function(){$('#animateAlert').show();});});
